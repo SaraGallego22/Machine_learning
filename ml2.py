cd9de8945723f1fd1a1afa01c293b843df2d2af5
SaraGallego22 <79612209+SaraGallego22@users.noreply.github.com>
2021-08-12 11:57:10 -0500

    Add files via upload

---
+++ ml2.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Jun 11 18:09:15 2021
+
+@author: ASUS
+"""
+
+from sklearn.datasets import load_iris
+iris = load_iris()
+print(list(iris.target_names))
